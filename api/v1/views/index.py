92746ef485f3406aa62811948d4273b8a6828e90
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-08-06 20:38:16 +0300

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,8 @@
 """My End-Points With their routes"""
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.engine.db_storage import classes
 
 @app_views.route('/status', strict_slashes=False)
 def Status():
@@ -11,3 +12,11 @@ def Status():
             "status": "OK"
             }
     return jsonify(data)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def Stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    return jsonify({
+        key: storage.count(value) for key, value in classes.items()
+    })
